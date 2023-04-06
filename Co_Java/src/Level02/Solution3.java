aad71f807657c75385081a928196c378a0a8c593
LeeHyunJin <myfamily11141@gmail.com>
2023-04-06 23:25:26 +0900

    2차 문제3(N과 M 사이의 짝수인 수의 제곱의 합)

--- Co_Java/src/Level02/Solution3.java
+++ Co_Java/src/Level02/Solution3.java
@@ -2,14 +2,21 @@ package Level02;
 
 class Solution3 {
 	public int solution(int N, int M) {
-		int total = 0;
-		
-		
+		int total=0;
+		for(int i=N; i<=M; i++) {
+			if(i%2==0) {
+				total += i*i;
+			}
+		}
 		return total;
 	}
 
 	public static void main(String[] args) {
-		
+		Solution3 sol = new Solution3();
+		int N = 4;
+		int M = 7;
+		int ret = sol.solution(N, M);
+		System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
 	}
 
 }
