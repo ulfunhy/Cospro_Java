8e36c411d1e835ef2c9719e3b8ffd42ccf93778d
LeeHyunJin <myfamily11141@gmail.com>
2023-04-08 23:54:56 +0900

    1차 문제3(일정 기간 구하기)

--- Co_Java/src/Level01/Solution3.java
+++ Co_Java/src/Level01/Solution3.java
@@ -12,12 +12,13 @@ class Solution3 {
 		total += day;
 		return total-1;		
 	}
+	
 	public int solution(int startMonth, int startDay, int endMonth, int endDay) {
 		int startTotal = func_a(startMonth, startDay);
 		int endTotal = func_a(endMonth, endDay);
-		
 		return endTotal - startTotal;
 	}
+	
 	public static void main(String[] args) {
 		Solution3 sol = new Solution3();
 		
